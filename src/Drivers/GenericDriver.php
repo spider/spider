580d3a316968b6042666e4731f7b701754859d39
chrismichaels84 <chrismichaels84@gmail.com>
2015-05-27 10:23:40 -0500

    generic driver implement interface

--- src/Drivers/GenericDriver.php
+++ src/Drivers/GenericDriver.php
@@ -10,11 +10,68 @@ namespace Michaels\Spider\Drivers;
 class GenericDriver implements DriverInterface
 {
 
-    /**
-     * Open a new connection
-     */
-    public function connect()
+    public function connect($properties)
     {
         // TODO: Implement connect() method.
+    }
+
+    public function listDbs()
+    {
+        // TODO: Implement listDbs() method.
+    }
+
+    public function openDb($database)
+    {
+        // TODO: Implement openDb() method.
+    }
+
+    public function closeDb()
+    {
+        // TODO: Implement closeDb() method.
+    }
+
+    public function addVertex($properties)
+    {
+        // TODO: Implement addVertex() method.
+    }
+
+    public function addEdge($from, $to, $properties)
+    {
+        // TODO: Implement addEdge() method.
+    }
+
+    public function updateVertex($id, $properties)
+    {
+        // TODO: Implement updateVertex() method.
+    }
+
+    public function getVertex($id)
+    {
+        // TODO: Implement getVertex() method.
+    }
+
+    public function getEdge($id)
+    {
+        // TODO: Implement getEdge() method.
+    }
+
+    public function dropVertex($id)
+    {
+        // TODO: Implement dropVertex() method.
+    }
+
+    public function dropEdge($id)
+    {
+        // TODO: Implement dropEdge() method.
+    }
+
+    public function statement($statement)
+    {
+        // TODO: Implement statement() method.
+    }
+
+    public function query($statement)
+    {
+        // TODO: Implement query() method.
     }
 }
