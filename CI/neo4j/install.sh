ef641ed19bad691ddd32bcc08b4b22646deaaec9
PommeVerte <dylan.millikin@gmail.com>
2015-08-19 16:18:08 -0400

    Corrected typo

--- CI/neo4j/install.sh
+++ CI/neo4j/install.sh
@@ -8,7 +8,7 @@ export JRE_HOME=/usr/lib/jvm/java-8-oracle
 wget -O $HOME/neo4j-community-$NEO4J_VERSION-unix.tar.gz dist.neo4j.org/neo4j-community-$NEO4J_VERSION-unix.tar.gz
 tar -xzf $HOME/neo4j-community-$NEO4J_VERSION-unix.tar.gz -C $HOME/
 $HOME/neo4j-community-$NEO4J_VERSION/bin/neo4j start
-sleep(10)
+sleep 10
 
 # changing password:
 curl -vX POST http://neo4j:neo4j@localhost:7474/user/neo4j/password -d"password=j4oen"
