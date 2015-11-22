adf3c54324854a8bcda251e5672a7d08ab9e93b6
Dylan Millikin <dylan.millikin@brightzone.fr>
2015-11-22 13:20:19 +0100

    change gremlin-server version for vagrant to 3.0.2, added composer.lock

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -3,7 +3,7 @@ echo "------------ PROVISIONING SYSTEM FROM BOOTSTRAP ------------"
 
 # Set variables
 export NEO4J_VERSION="2.2.4"
-export GREMLINSERVER_VERSION="3.0.0"
+export GREMLINSERVER_VERSION="3.0.2"
 export ORIENT_VERSION="2.1.0"
 
 export INSTALL_DIR="/home/vagrant"
