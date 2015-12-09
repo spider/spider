964b1eb3c9533648e5620a868d78e5eae3e824b0
chrismichaels84 <chrismichaels84@gmail.com>
2015-12-09 14:46:14 -0600

    switch out scotchbox

--- vagrant/startup.sh
+++ vagrant/startup.sh
@@ -2,14 +2,6 @@
 echo "------------ RUNNING STARTUP COMMANDS ------------"
 
 ### Set variables
-export NEO4J_VERSION="2.2.4"
-export GREMLINSERVER_VERSION="3.0.2"
-export ORIENT_VERSION="2.1.6"
-
-export INSTALL_DIR="/home/vagrant"
-export VAGRANT_DIR="/vagrant"
-export BOOTSTRAP_DIR="$VAGRANT_DIR/vagrant"
-
 export GREMLIN_DIR="apache-gremlin-server-$GREMLINSERVER_VERSION-incubating"
 
 ## start gremlin-server
@@ -44,12 +36,3 @@ sudo nohup $INSTALL_DIR/orientdb-community-$ORIENT_VERSION/bin/orientdb.sh start
 sleep 15
 
 echo "------------ END: RUNNING STARTUP COMMANDS ------------"
-
-#if [ -f /home/vagrant/gremlin-server-spider.yaml ];
-#then
-#   echo "yes."
-#   touch /home/vagrant/still.exists.txt
-#else
-#   echo "no."
-#   touch /home/vagrant/doesnotexist.txt
-#fi
