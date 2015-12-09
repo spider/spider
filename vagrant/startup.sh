f3a36e427ad58b9eb2534a4d279e92e6370e9f4e
chrismichaels84 <chrismichaels84@gmail.com>
2015-12-09 16:14:53 -0600

    fix startup scripts

--- vagrant/startup.sh
+++ vagrant/startup.sh
@@ -2,6 +2,15 @@
 echo "------------ RUNNING STARTUP COMMANDS ------------"
 
 ### Set variables
+export NEO4J_VERSION="2.2.4"
+export GREMLINSERVER_VERSION="3.0.2"
+export ORIENT_VERSION="2.1.6"
+
+export INSTALL_DIR="/home/vagrant"
+export VAGRANT_DIR="/vagrant"
+export BOOTSTRAP_DIR="$VAGRANT_DIR/vagrant"
+export INITIAL_BUILD_DIR=$(pwd)
+
 export GREMLIN_DIR="apache-gremlin-server-$GREMLINSERVER_VERSION-incubating"
 
 ## start gremlin-server
