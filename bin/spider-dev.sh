71f8aa2ff88d967f3c6e4f9f285b21d8774b182a
Michael Wilson <chrismichaels84@users.noreply.github.com>
2016-05-18 13:48:33 -0500

    #123: Initialize basic shell script for docker management

--- bin/spider-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Uses flags or defaults to set environment variables for versions
-# before executing `docker-compose up -d`
-
-# looks like
-# `ORIENT_VERSION=2.1.6; NEO4J_VERSION=1.7.3; PHP_VERSION=5.6; docker-compose up -d` to start services
-# `docker-compose run php "cd /spider; vendor/bin/phpunit"` to run all tests
